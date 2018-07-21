16dec0303bd5151077dea93e40a7602f2184298e
headHUB <james@theheadspot.com>
2018-07-21 01:01:05 -0500

    took out servo controller added uugear communication

--- pantilt.py
+++ pantilt.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python
 
-import smbus
-import time
+from time import sleep
 
-bus = smbus.SMBus(1)
-addr = 0x40
+import UUGear
+
+UUGear.UUGearDevice.setShowLogs(0)
+
+device = UUGear.UUGearDevice('')
+
+if device.isValid():
+
+    device.attachServo(4)
+    device.attachServo(5)
+
+    sleep(3)
+
+else:
+
+    print 'UUGear device is not correctly initialized.'
+
 
 def scale(x, in_min, in_max, out_min, out_max):
-	return (x - in_min)*(out_max - out_min)/(in_max - in_min) + out_min
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-## enable the PC9685 and enable autoincrement
-bus.write_byte_data(addr, 0, 0x20)
-bus.write_byte_data(addr, 0xfe, 0x1e)
-
-bus.write_word_data(addr, 0x06, 0)
-bus.write_word_data(addr, 0x08, 1250)
-
-bus.write_word_data(addr, 0x0a, 0)
-bus.write_word_data(addr, 0x0c, 1250)
 
 while True:
-	pipein = open("/var/www/html/FIFO_pipan", 'r')
-	line = pipein.readline()
-	line_array = line.split(' ')
-	if line_array[0] == "servo":
-		pan_setting = scale(int(line_array[1]), 80, 220, 833, 1667)
-		tilt_setting = scale(int(line_array[2]), 50, 250, 833, 1667)
-		bus.write_word_data(addr, 0x08, pan_setting)
-		bus.write_word_data(addr, 0x0c, tilt_setting)
-	pipein.close()
+
+    pipein = open("/var/www/html/FIFO_pipan", 'r')
+    line = pipein.readline()
+    line_array = line.split(' ')
+    if line_array[0] == "servo":
+        pan_setting = scale(int(line_array[1]), 80, 220, 0, 180)
+        tilt_setting = scale(int(line_array[2]), 50, 250, 0, 180)
+        device.writeServo(UUGear - Arduino - 9886 - 99474, pan_setting)
+        device.writeServo(4, tilt_setting)
+    pipein.close()
